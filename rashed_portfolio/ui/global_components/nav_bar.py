6051295136ef761abff1b0a30415d2e801d9bb0f
Mahmudur Rahman Shovon <mars@Mahmudurs-Mac-Studio.local>
2024-05-01 00:04:41 +0600

    modularized

--- rashed_portfolio/ui/global_components/nav_bar.py
+++ rashed_portfolio/ui/global_components/nav_bar.py
@@ -2,16 +2,10 @@ import reflex as rx
 from typing import Optional
 
 from rashed_portfolio.data.constants import ProfileData
-from rashed_portfolio.ui.constants.ui_constants import MENU_ITEMS, STARTING_COLOR, ENDING_COLOR
+from rashed_portfolio.ui.constants.ui_constants import MENU_ITEMS, STARTING_COLOR
 
 
 def navbar(active_item: Optional[str] = None):
-    def apply_color(item: str):
-        if (active_item and active_item.lower() == item.lower()):
-            return STARTING_COLOR
-        else:
-            return 'black'
-        
     return rx.container(
         rx.hstack(
             # Logo
@@ -26,12 +20,7 @@ def navbar(active_item: Optional[str] = None):
                 height='4.5em'
             ),
             rx.spacer(),
-            rx.center(
-                rx.hstack(
-                    *[rx.text(item, font_size="1.25em", padding_left="10px", color=apply_color(item), display=["none", "none", "none", "flex", "flex"],) for item in MENU_ITEMS],
-                ),
-                height='4.5em',
-            ),
+            menu_items(active_item),
             rx.center(
                 rx.icon_button(
                     rx.icon(
@@ -44,6 +33,31 @@ def navbar(active_item: Optional[str] = None):
                 ),
                 height='4.5rem',
             )
+        ),
+        height='4.5em',
+    )
+
+
+def menu_items(active_item):
+    def apply_color(item: str):
+        if (active_item and active_item.lower() == item.lower()):
+            return STARTING_COLOR
+        else:
+            return 'black'
+        
+    items = []
+    for item in MENU_ITEMS:
+        items.append(rx.text(
+            item,
+            font_size='1.25em',
+            padding_left='10px',
+            color=apply_color(item),
+        ))
+
+    return rx.center(
+        rx.hstack(
+            *items,
+            display=["none", "none", "none", "flex", "flex"],
         ),
         height='4.5em',
     )
